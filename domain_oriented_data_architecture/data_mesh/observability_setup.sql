a5ff601988b4f81fdb890c27b8016ac25e1ed214
Raghavendra H S <raghavendra.shivaswamy56@gmail.com>
2025-04-27 13:18:28 +0530

    Create observability_setup.sql

---
+++ domain_oriented_data_architecture/data_mesh/observability_setup.sql
@@ -0,0 +1,8 @@
+# Observability Setup (Basic Metrics)
+
+-- Create a view that tracks the volume and last load timestamp
+CREATE OR REPLACE VIEW retail_domain.orders.orders_observability AS
+SELECT
+  count(*) as total_records,
+  max(order_date) as latest_order_date
+FROM retail_orders;
